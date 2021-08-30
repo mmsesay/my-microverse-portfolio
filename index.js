331711e78f6a3e82b97bc143b41a322f85f30653
Goldinium <badrbenslama8@gmail.com>
2021-08-30 14:12:22 +0100

    add close font awesome icon, and event listener to close the menu

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ function toggleMobileMenu() {
   // target the element
   let hamburgerButton = document.getElementById('hamburgerButton');
   let mobileMenu = document.getElementById('mobile-menu');
+  let times = document.getElementById('times');
 
   // add event listener
   hamburgerButton.addEventListener('click', function() {
     mobileMenu.style.display = "block";
+    hamburgerButton.style.display = "none";
+    times.style.display = "block";
   });
+
+  times.addEventListener('click', function() {
+    mobileMenu.style.display = "none";
+    hamburgerButton.style.display = "block";
+    times.style.display = "none";
+  })
 }
