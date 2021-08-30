e58a367255452d356a9b627b57a95eaf005fbff2
mmsesay <maej_01@hotmail.com>
2021-08-30 15:20:55 +0000

    Add: functionality to hide the mobile menu when a user click on the options

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 // index.js
+let mobileMenu = document.getElementById('mobile-menu');
+let hamburgerButton = document.getElementById('hamburgerButton');
+let times = document.getElementById('times');
 
-function toggleMobileMenu() {
-  // target the element
-  let hamburgerButton = document.getElementById('hamburgerButton');
-  let mobileMenu = document.getElementById('mobile-menu');
-  let times = document.getElementById('times');
-
-  // add event listener
-  hamburgerButton.addEventListener('click', function() {
-    mobileMenu.style.display = "block";
-    hamburgerButton.style.display = "none";
-    times.style.display = "block";
-  });
-
-  times.addEventListener('click', function() {
-    mobileMenu.style.display = "none";
-    hamburgerButton.style.display = "block";
+document.querySelectorAll('.mobileMenuOption').forEach((item) => {
+  item.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
     times.style.display = "none";
-  })
-}
+  });
+});
+
+// add event listener
+hamburgerButton.addEventListener('click', function() {
+  mobileMenu.style.display = "block";
+  hamburgerButton.style.display = "none";
+  times.style.display = "block";
+});
+
+times.addEventListener('click', function() {
+  mobileMenu.style.display = "none";
+  hamburgerButton.style.display = "block";
+  times.style.display = "none";
+});
