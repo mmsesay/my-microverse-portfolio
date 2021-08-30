cb47e5bca5a60c6639dbb2dc666b0048b5d548d5
mmsesay <maej_01@hotmail.com>
2021-08-30 17:51:55 +0000

    Refactor: the entire js script to remove duplicates

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 // index.js
+let body = document.querySelector('body');
 let mobileMenu = document.getElementById('mobile-menu');
-let hamburgerButton = document.getElementById('hamburgerButton');
-let times = document.getElementById('times');
+let openButton = document.getElementById('hamburgerButton');
+let closeButton = document.getElementById('times');
 
+// hides elements
+function hide(args) {
+  args.forEach((arg) => {
+    arg.style.display = "none";
+  })
+}
+
+// show elements
+function show(args) {
+  args.forEach((arg) => {
+    arg.style.display = "block";
+  })
+}
+
+function closeMobileMenu() {
+  hide([mobileMenu, closeButton]);
+  show([openButton]);
+  toggleScroll(false); // makes the body to scroll
+}
+
+function openMobileMenu() {
+  hide([openButton]);
+  show([mobileMenu, closeButton]);
+  toggleScroll(true); // makes the body non-scrollable
+}
+
+// toggle body scrolling
+function toggleScroll(state) {
+  // param state: is a boolean
+  state? `${body.style.overflow = 'hidden'}`: `${body.style.overflow = 'scroll'}`;
+}
+
+// target all mobile menu options
 document.querySelectorAll('.mobileMenuOption').forEach((item) => {
-  item.addEventListener('click', () => {
-    mobileMenu.style.display = 'none';
-    times.style.display = "none";
-  });
+  item.addEventListener('click', closeMobileMenu);
 });
 
-// add event listener
-hamburgerButton.addEventListener('click', function() {
-  mobileMenu.style.display = "block";
-  hamburgerButton.style.display = "none";
-  times.style.display = "block";
-});
+// open button event listener
+openButton.addEventListener('click', openMobileMenu);
 
-times.addEventListener('click', function() {
-  mobileMenu.style.display = "none";
-  hamburgerButton.style.display = "block";
-  times.style.display = "none";
-});
+// close button event listener
+closeButton.addEventListener('click', closeMobileMenu);
