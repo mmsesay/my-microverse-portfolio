9193adb01f5b7429b2c816179715ec389d4cb4ad
mmsesay <maej_01@hotmail.com>
2021-08-31 18:15:09 +0000

    Fix: popup window overflowing

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const openButton = document.getElementById('hamburgerButton');
 const closeButton = document.getElementById('times');
 const popupContainer = document.querySelector('.popup-container');
 const openPopupBtn = document.getElementById('openPopupWindow');
+const projectsContainer =  document.querySelector('.grid-container');
+const closePopupIcon = document.getElementById('closePopupIcon');
 
 // hides elements
 function hide(args) {
@@ -32,9 +34,14 @@ function openMobileMenu() {
   body.style.overflow = 'hidden';
 }
 
-function openPopupWindow() {
+function popupWindow() {
   show([popupContainer]);
-  // body.style.overflow = 'hidden';
+  body.style.overflow = 'hidden';
+}
+
+function closePopupWindow() {
+  hide([popupContainer]);
+  body.style.overflow = 'scroll';
 }
 
 // target all mobile menu options
@@ -44,7 +51,7 @@ document.querySelectorAll('.mobileMenuOption').forEach((item) => {
 
 // target all buttons of projects
 document.querySelectorAll('.openPopupWindow').forEach((item) => {
-  item.addEventListener('click', openPopupWindow);
+  item.addEventListener('click', popupWindow);
 });
 
 // open button event listener
@@ -52,3 +59,60 @@ openButton.addEventListener('click', openMobileMenu);
 
 // close button event listener
 closeButton.addEventListener('click', closeMobileMenu);
+
+
+// Projects Dynamic data
+const workProjects = [
+  {
+    title: 'Profesional Art Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard`,
+    image: {
+      imageUrl: './img/wesing.png',
+      imageAlt: 'Mobile Snapshot',
+    },
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    seeLive: 'https://mmsesay.github.io/my-microverse-portfolio/',
+    sourceUrl: 'https://github.com/mmsesay/my-microverse-portfolio',
+  },
+  {
+    title: 'Profesional Art Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard`,
+    image: {
+      imageUrl: './img/todo.png',
+      imageAlt: 'ToDo List',
+    },
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    seeLive: 'https://mmsesay.github.io/my-microverse-portfolio/',
+    sourceUrl: 'https://github.com/mmsesay/my-microverse-portfolio',
+  }
+];
+
+let projectHTML = '';
+
+workProjects.forEach((project, index) => {  
+  projectHTML += `
+    <div class="grid-items d-flex grid-item-1">
+      <div class="grid-item-card text-white">
+        <div class="grid-item-card-content d-flex show-on-small hide-on-large">
+          <h2 class="font-crete-round">${project.title}</h2>
+          <p class="font-inter">${project.description}</p>
+          <div class="box-container d-flex">
+            <ul class="d-flex font-inter list-none">`;
+            project.technologies.forEach(tech => {
+              projectHTML += `<li class="box">${tech}</li>`
+            });
+  projectHTML += `</ul>
+          </div>
+        </div>
+        <div class="button-container">
+          <a
+          class="font-inter text-white no-underline primary-button primary-color btn openPopupWindow"
+          rel="noopener noreferrer"
+          >See Project</a>
+        </div>
+      </div>
+    </div>`
+});
+
+projectsContainer.innerHTML = projectHTML;
+
